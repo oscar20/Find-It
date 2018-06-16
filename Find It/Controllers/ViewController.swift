8858f88386e2c7f53978fa2592a8c0759e342da5
oscar20 <oscar_almazan@outlook.com>
2018-06-15 20:29:13 -0500

    Peticion básica..

---
+++ Find It/Controllers/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Find It
+//
+//  Created by d182_oscar_a on 15/06/18.
+//  Copyright © 2018 d182_oscar_a. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.lightGray
+        
+        //Peticion a mi API
+        let url = URL(string: "https://api.goodzer.com/products/v0.1/location_details/?locationId=ffy5Pc7R&apiKey=")
+        
+        //Ejecucion de tarea
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if data != nil {
+                print("Datos no vacios")
+                print(data!)
+                print(response!)
+                
+            }else{
+                print("Datos nulos")
+            }
+        }
+        task.resume()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
