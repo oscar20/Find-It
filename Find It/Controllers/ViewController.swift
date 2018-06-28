660399d756f2d2fc554384b2a36e7b2dc119dd40
oscar20 <oscar_almazan@outlok.com>
2018-06-27 21:57:24 -0500

    Se logra mandar validar espacios en blanco y tabuladores

--- Find It/Controllers/ViewController.swift
+++ Find It/Controllers/ViewController.swift
@@ -10,19 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    let productLabel : UILabel = {
+        let lbl = UILabel()
+        lbl.text = "¿Què producto estás buscando?"
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
+    
     let registerButton: UIButton = {
         
         let btn = UIButton(type: .system)
         btn.backgroundColor = UIColor(red: 232/255, green: 173/255, blue: 72/255, alpha: 1.0)
         btn.translatesAutoresizingMaskIntoConstraints = false
         btn.setTitleColor(UIColor.white, for: .normal)
-        btn.setTitle("Recupera datos", for: .normal)
+        btn.setTitle("Buscar", for: .normal)
         btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
         btn.layer.cornerRadius = 5
         btn.layer.masksToBounds = true
-        //btn.addTarget(self, action: #selector(recuperaDatos), for: .touchUpInside)
         btn.addTarget(self, action: #selector(getStores), for: .touchUpInside)
-         return btn
+        return btn
         
     }()
     
@@ -40,17 +47,23 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor.white
         view.addSubview(registerButton)
         view.addSubview(parametro)
+        view.addSubview(productLabel)
         
-        registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        registerButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        productLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        productLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
         
-        parametro.topAnchor.constraint(equalTo: registerButton.bottomAnchor, constant: 30).isActive = true
+        parametro.topAnchor.constraint(equalTo: productLabel.bottomAnchor, constant: 30).isActive = true
         parametro.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        registerButton.topAnchor.constraint(equalTo: parametro.bottomAnchor, constant: 30).isActive = true
+        registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        
     }
     
     @objc func recuperaDatos(){
         print("Func recuperaDatos")
-        let apiKey = ""
+        let apiKey = "4ee7cdf8c6a05e6f91f8077f3bd003ba"
         let urlString = "https://api.goodzer.com/products/v0.1/location_details/?locationId=\(String(describing: parametro.text!))&apiKey=\(apiKey)"
         let url = URL(string: urlString)
         URLSession.shared.dataTask(with: url!) { (data, response, error) in
@@ -75,31 +88,42 @@ class ViewController: UIViewController {
     }
     
     @objc func getStores(){
-        let latitud = "40.714353"
-        let longitud = "-74.005973"
-        print("Entrando a metodo pararecuperar tiendas cercanas")
-        let apiKey = ""
-        let urlString = "https://api.goodzer.com/products/v0.1/search_stores/?query=\(parametro.text!)&lat=\(latitud)&lng=\(longitud)&radius=5&priceRange=30:120&apiKey=\(apiKey)"
-        let url = URL(string: urlString)
-        URLSession.shared.dataTask(with: url!) { (data, response, error) in
-            print("Obteniendo tiendas con cierto producto")
-            guard let data = data else {return}
-            
-            //Parseo...
-            do{
-                let productStore = try JSONDecoder().decode(PeticionProducto.self, from: data)
-                print("Status: \(String(describing: productStore.status))")
-                print("WebSite de primer tienda: \(String(describing: productStore.stores?.first?.website))")
-                print("Stores_found: \(String(describing: productStore.stores_found))")
-                print("Time: \(String(describing: productStore.time))")
-            }catch let error{
-                print("Error: ", error)
-            }
-            
-        }.resume()
+        //antes de armar cualquier peticion debo validar si lo que ingreso en el text field es valido
+        
+        guard let miParametro = parametro.text else {return}
+        let trimmed  = miParametro.trimmingCharacters(in: .whitespaces)
+        print(trimmed)
+        let formatoCadena = trimmed.replacingOccurrences(of: " ", with: "+")
+        if formatoCadena.isEmpty{
+            print("Cadena vacia...")
+        }else{
+            print("..\(formatoCadena)..")
+            let latitud = "40.714353"
+            let longitud = "-74.005973"
+            print("Entrando a metodo pararecuperar tiendas cercanas")
+            let apiKey = ""
+            let urlString = "https://api.goodzer.com/products/v0.1/search_stores/?query=\(formatoCadena)&lat=\(latitud)&lng=\(longitud)&radius=5&priceRange=30:120&apiKey=\(apiKey)"
+            let url = URL(string: urlString)
+            URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                print("Obteniendo tiendas con cierto producto")
+                guard let data = data else {return}
+                
+                //Parseo...
+                do{
+                    let productStore = try JSONDecoder().decode(PeticionProducto.self, from: data)
+                    if productStore.stores_found == nil || productStore.stores_found! == 0 {
+                        print("No se encontro ninguna tienda con el producto")
+                    }else{
+                        print("Status: \(String(describing: productStore.status))")
+                        print("WebSite de primer tienda: \(String(describing: productStore.stores?.first?.website))")
+                        print("Stores_found: \(String(describing: productStore.stores_found))")
+                        print("Time: \(String(describing: productStore.time))")
+                    }
+                }catch let error{
+                    print("Error: ", error)
+                }
+            }.resume()
+        }
     }
-
-
-
 }
 
