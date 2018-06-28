a2bc4f11fafe12e86ce82d59547667d2ece57e0a
oscar20 <oscar_almazan@outlok.com>
2018-06-27 23:13:25 -0500

    Dismiss keyboard, labels lat, long

--- Find It/Controllers/ViewController.swift
+++ Find It/Controllers/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
+    
     
     
     let productLabel : UILabel = {
@@ -38,19 +39,36 @@ class ViewController: UIViewController {
         p.text = "v-neck+sweater"
         p.backgroundColor = UIColor.yellow
         p.translatesAutoresizingMaskIntoConstraints = false
+        p.keyboardType = UIKeyboardType.default
         return p
     }()
     
+    let labelLatitud: UILabel = {
+       let labelLat = UILabel()
+        labelLat.text = "Latitud"
+        labelLat.translatesAutoresizingMaskIntoConstraints = false
+        return labelLat
+    }()
+    
+    let labelLongitud: UILabel = {
+       let labelLong = UILabel()
+        labelLong.text = "Longitud"
+        labelLong.translatesAutoresizingMaskIntoConstraints = false
+        return labelLong
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.parametro.delegate = self
         view.backgroundColor = UIColor.white
         view.addSubview(registerButton)
         view.addSubview(parametro)
         view.addSubview(productLabel)
+        view.addSubview(labelLatitud)
+        view.addSubview(labelLongitud)
         
         productLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        productLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        productLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 250).isActive = true
         
         parametro.topAnchor.constraint(equalTo: productLabel.bottomAnchor, constant: 30).isActive = true
         parametro.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -58,56 +76,34 @@ class ViewController: UIViewController {
         registerButton.topAnchor.constraint(equalTo: parametro.bottomAnchor, constant: 30).isActive = true
         registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
+        labelLatitud.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        labelLatitud.topAnchor.constraint(equalTo: registerButton.bottomAnchor, constant: 30).isActive = true
+        
+        labelLongitud.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        labelLongitud.topAnchor.constraint(equalTo: labelLatitud.bottomAnchor, constant: 30).isActive = true
         
     }
     
-    @objc func recuperaDatos(){
-        print("Func recuperaDatos")
-        let apiKey = "4ee7cdf8c6a05e6f91f8077f3bd003ba"
-        let urlString = "https://api.goodzer.com/products/v0.1/location_details/?locationId=\(String(describing: parametro.text!))&apiKey=\(apiKey)"
-        let url = URL(string: urlString)
-        URLSession.shared.dataTask(with: url!) { (data, response, error) in
-            print("Fetching data...")
-            guard let data = data else {return}
-            
-        //Parseo de Json
-            do {
-                let store = try JSONDecoder().decode(Store.self, from: data)
-                print("Status de peticion...\(String(describing: store.status))")
-                print("Address: \(String(describing: store.location?.address))")
-                print("City: \(String(describing: store.location?.city))")
-                print("Latitud: \(String(describing: store.location?.lat))")
-                print("Longitud: \(String(describing: store.location?.lng))")
-            }catch let error{
-                print("Error:", error)
-            }
-                
-            
-        }.resume()
-        
-    }
     
     @objc func getStores(){
-        //antes de armar cualquier peticion debo validar si lo que ingreso en el text field es valido
-        
-        guard let miParametro = parametro.text else {return}
-        let trimmed  = miParametro.trimmingCharacters(in: .whitespaces)
-        print(trimmed)
-        let formatoCadena = trimmed.replacingOccurrences(of: " ", with: "+")
-        if formatoCadena.isEmpty{
-            print("Cadena vacia...")
+        print("Running getStores...")
+        guard let textTextField = parametro.text else {return}//Valido que mi TextField contenga una cadena.
+        let textTextField_trimmed  = textTextField.trimmingCharacters(in: .whitespaces) //Recorto la cadena.
+        let formattingText = textTextField_trimmed.replacingOccurrences(of: " ", with: "+") //Reemplazando espacios en blanco.
+        if formattingText.isEmpty{
+            print("Empty string...")
         }else{
-            print("..\(formatoCadena)..")
+            print("The parameter is:\(formattingText).")
             let latitud = "40.714353"
             let longitud = "-74.005973"
-            print("Entrando a metodo pararecuperar tiendas cercanas")
             let apiKey = ""
-            let urlString = "https://api.goodzer.com/products/v0.1/search_stores/?query=\(formatoCadena)&lat=\(latitud)&lng=\(longitud)&radius=5&priceRange=30:120&apiKey=\(apiKey)"
-            let url = URL(string: urlString)
+            let urlString = "https://api.goodzer.com/products/v0.1/search_stores/?query=\(formattingText)&lat=\(latitud)&lng=\(longitud)&radius=5&priceRange=30:120&apiKey=\(apiKey)" //Armo mi URL para la peticion.
+            let url = URL(string: urlString) //Ejecuto mi peticion
+            labelLatitud.text = latitud
+            labelLongitud.text = longitud
             URLSession.shared.dataTask(with: url!) { (data, response, error) in
-                print("Obteniendo tiendas con cierto producto")
+                print("Getting stores...")
                 guard let data = data else {return}
-                
                 //Parseo...
                 do{
                     let productStore = try JSONDecoder().decode(PeticionProducto.self, from: data)
@@ -125,5 +121,43 @@ class ViewController: UIViewController {
             }.resume()
         }
     }
+    
+    //Dismiss keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        parametro.resignFirstResponder() //Renuncia a tu estado de primera ventana.
+        return true
+    }
+    
+    
+    /*@objc func recuperaDatos(){
+     print("Func recuperaDatos")
+     let apiKey = ""
+     let urlString = "https://api.goodzer.com/products/v0.1/location_details/?locationId=\(String(describing: parametro.text!))&apiKey=\(apiKey)"
+     let url = URL(string: urlString)
+     URLSession.shared.dataTask(with: url!) { (data, response, error) in
+     print("Fetching data...")
+     guard let data = data else {return}
+     
+     //Parseo de Json
+     do {
+     let store = try JSONDecoder().decode(Store.self, from: data)
+     print("Status de peticion...\(String(describing: store.status))")
+     print("Address: \(String(describing: store.location?.address))")
+     print("City: \(String(describing: store.location?.city))")
+     print("Latitud: \(String(describing: store.location?.lat))")
+     print("Longitud: \(String(describing: store.location?.lng))")
+     }catch let error{
+     print("Error:", error)
+     }
+     
+     
+     }.resume()
+     
+     }
+     */
 }
 
